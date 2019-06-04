d66cfce75bd2e1e36f624716ad7becb84b05f16e
amilonurb <brunolima.18@outlook.com>
2019-06-04 01:16:43 -0300

    BD H2 e criação da base de dados

--- src/main/java/br/com/brlima/cursomc/model/Categoria.java
+++ src/main/java/br/com/brlima/cursomc/model/Categoria.java
@@ -2,15 +2,25 @@ package br.com.brlima.cursomc.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@Entity
 @Data
 @AllArgsConstructor
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 5009143847763774982L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@EqualsAndHashCode.Exclude
 	private String nome;
 }
