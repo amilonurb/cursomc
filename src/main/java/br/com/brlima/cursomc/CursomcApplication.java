0e086a5fd31f183ebd322fec7b20d448c299b4ed
amilonurb <brunolima.18@outlook.com>
2019-06-04 01:41:38 -0300

    Instanciação de categorias na execução

--- src/main/java/br/com/brlima/cursomc/CursomcApplication.java
+++ src/main/java/br/com/brlima/cursomc/CursomcApplication.java
@@ -1,11 +1,29 @@
 package br.com.brlima.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.brlima.cursomc.model.Categoria;
+import br.com.brlima.cursomc.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática, meu");
+		Categoria cat2 = new Categoria(null, "Escritório, jão");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
