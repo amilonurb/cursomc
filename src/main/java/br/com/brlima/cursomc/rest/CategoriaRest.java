167f7d3803bb77a33c667f4c8ff990ed664c57ea
amilonurb <brunolima.18@outlook.com>
2019-06-09 17:47:54 -0300

    Add inserção de Categoria via POST

--- src/main/java/br/com/brlima/cursomc/rest/CategoriaRest.java
+++ src/main/java/br/com/brlima/cursomc/rest/CategoriaRest.java
@@ -1,11 +1,16 @@
 package br.com.brlima.cursomc.rest;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.brlima.cursomc.model.produto.Categoria;
 import br.com.brlima.cursomc.service.CategoriaService;
@@ -21,5 +26,12 @@ public class CategoriaRest {
 	public ResponseEntity<Categoria> find(@PathVariable("id") Long id) {
 		Categoria categoria = service.find(id);
 		return ResponseEntity.ok().body(categoria);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
