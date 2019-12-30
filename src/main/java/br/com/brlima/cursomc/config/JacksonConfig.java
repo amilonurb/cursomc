648cce34221065af274a972038b3d16b8e711a53
amilonurb <brunolima.18@outlook.com>
2019-12-30 03:04:01 -0300

    identificando bean

--- src/main/java/br/com/brlima/cursomc/config/JacksonConfig.java
+++ src/main/java/br/com/brlima/cursomc/config/JacksonConfig.java
@@ -2,6 +2,7 @@ package br.com.brlima.cursomc.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
@@ -11,6 +12,7 @@ import br.com.brlima.cursomc.model.pagamento.PagamentoCartao;
 @Configuration
 public class JacksonConfig {
 
+    @Bean
     public Jackson2ObjectMapperBuilder objectMapperBuilder() {
 
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
