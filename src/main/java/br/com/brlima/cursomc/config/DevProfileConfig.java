eb1264844ca26f44d319fc4d08c8f91c47e196a2
amilonurb <brunolima.18@outlook.com>
2019-12-31 02:09:29 -0300

    Corrigindo Bean

--- src/main/java/br/com/brlima/cursomc/config/DevProfileConfig.java
+++ src/main/java/br/com/brlima/cursomc/config/DevProfileConfig.java
@@ -2,6 +2,7 @@ package br.com.brlima.cursomc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -17,6 +18,7 @@ public class DevProfileConfig {
     @Value("${spring.jpa.hibernate.ddl-auto}")
     private String strategy;
 
+    @Bean
     public boolean instantiateDatabase() {
         if (!strategy.equals("create")) {
             return false;
