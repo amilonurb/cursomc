ef33c1c0a7e01cf524374a70844fec7fc1c27f8f
amilonurb <brunolima.18@outlook.com>
2020-01-16 23:02:38 -0300

    Tratamento personalizado de exceções do S3

--- src/main/java/br/com/brlima/cursomc/rest/exception/RestExceptionHandler.java
+++ src/main/java/br/com/brlima/cursomc/rest/exception/RestExceptionHandler.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import br.com.brlima.cursomc.service.exception.AuthorizationException;
 import br.com.brlima.cursomc.service.exception.DataIntegrityException;
+import br.com.brlima.cursomc.service.exception.FileException;
 import br.com.brlima.cursomc.service.exception.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -36,6 +41,31 @@ public class RestExceptionHandler {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
     }
 
+    @ExceptionHandler(FileException.class)
+    public ResponseEntity<StandardError> handleFile(FileException exception, HttpServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(AmazonServiceException.class)
+    public ResponseEntity<StandardError> handleAmazonService(AmazonServiceException exception, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.valueOf(exception.getStatusCode());
+        StandardError error = new StandardError(System.currentTimeMillis(), httpStatus.value(), httpStatus.getReasonPhrase(), exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(httpStatus).body(error);
+    }
+
+    @ExceptionHandler(AmazonClientException.class)
+    public ResponseEntity<StandardError> handleAmazonClient(AmazonClientException exception, HttpServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(AmazonS3Exception.class)
+    public ResponseEntity<StandardError> handleAmazonS3(AmazonS3Exception exception, HttpServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<StandardError> handleValidation(MethodArgumentNotValidException exception, HttpServletRequest request) {
         ValidationError validationError = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de Validação", exception.getMessage(), request.getRequestURI());
@@ -45,5 +75,4 @@ public class RestExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(validationError);
     }
-
 }
