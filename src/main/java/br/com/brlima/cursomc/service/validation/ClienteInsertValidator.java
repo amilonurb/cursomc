df041bcbdaea29cea84197e2ff177aad31c627ad
amilonurb <brunolima.18@outlook.com>
2020-01-26 00:15:53 -0300

    Trocando service por repository

--- src/main/java/br/com/brlima/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/brlima/cursomc/service/validation/ClienteInsertValidator.java
@@ -8,39 +8,39 @@ import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import br.com.brlima.cursomc.model.cliente.Cliente;
 import br.com.brlima.cursomc.model.cliente.dto.ClienteNewDTO;
 import br.com.brlima.cursomc.model.enums.TipoCliente;
+import br.com.brlima.cursomc.repository.ClienteRepository;
 import br.com.brlima.cursomc.rest.exception.FieldMessage;
-import br.com.brlima.cursomc.service.ClienteService;
 import br.com.brlima.cursomc.util.DocumentUtils;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
     @Autowired
-    private ClienteService clienteService;
-    
+    private ClienteRepository clienteRepository;
+
     @Override
     public boolean isValid(ClienteNewDTO dto, ConstraintValidatorContext context) {
         List<FieldMessage> erros = new ArrayList<>();
 
-        if (dto.getCodigoTipoCliente().equals(TipoCliente.PESSOA_FISICA.getKey()) && !DocumentUtils.isValidCPF(dto.getCpfOuCnpj())) {
+        if (dto.getCodigoTipoCliente().equals(TipoCliente.PESSOA_FISICA.getKey())
+                && !DocumentUtils.isValidCPF(dto.getCpfOuCnpj())) {
             erros.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
         }
 
-        if (dto.getCodigoTipoCliente().equals(TipoCliente.PESSOA_JURIDICA.getKey()) && !DocumentUtils.isValidCNPJ(dto.getCpfOuCnpj())) {
+        if (dto.getCodigoTipoCliente().equals(TipoCliente.PESSOA_JURIDICA.getKey())
+                && !DocumentUtils.isValidCNPJ(dto.getCpfOuCnpj())) {
             erros.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
         }
-        
-        Cliente cliente = clienteService.findByEmail(dto.getEmail());
-        if (cliente != null) {
-            erros.add(new FieldMessage("email", "Email já existente"));
+
+        if (clienteRepository.findByEmail(dto.getEmail()).isPresent()) {
+            erros.add(new FieldMessage("email", "E-mail já existente"));
         }
 
         erros.forEach(fm -> {
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(fm.getErrorMessage())
-                    .addPropertyNode(fm.getFieldName())
+            context.buildConstraintViolationWithTemplate(fm.getErrorMessage())//
+                    .addPropertyNode(fm.getFieldName())//
                     .addConstraintViolation();
         });
         return erros.isEmpty();
